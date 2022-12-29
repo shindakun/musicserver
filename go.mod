72df98fb52a1d2d7b68bcc0dca93537bc075a500
Steve Layton <shindakun@users.noreply.github.com>
2022-12-28 19:00:28 -0800

    adding music title announcement and maybe start to bot greatings

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/shindakun/musicserver
 
 go 1.19
+
+require (
+	github.com/gempir/go-twitch-irc/v3 v3.2.0 // indirect
+	github.com/shindakun/envy v1.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"os"
+	"strings"
 	"time"
+
+	"github.com/gempir/go-twitch-irc/v3"
+	"github.com/shindakun/envy"
 )
 
-func getRandFile() string {
+var client *twitch.Client
+
+func getRandFile() (string, error) {
 	d, err := os.ReadDir("./music")
 	if err != nil {
 		panic(err)
@@ -16,9 +23,18 @@ func getRandFile() string {
 
 	n := rand.Intn(len(d)+1) - 1
 
-	s := "http://localhost:8080/music/" + d[n].Name()
+	if !d[n].IsDir() {
+		s := "http://localhost:8080/music/" + d[n].Name()
 
-	return s
+		name := strings.ReplaceAll(d[n].Name(), ".", " ")
+
+		log.Println(name)
+
+		client.Say("shindakun", "Now playing: "+name)
+
+		return s, nil
+	}
+	return "", fmt.Errorf("is dir")
 }
 
 func cors(fs http.Handler) http.HandlerFunc {
@@ -31,16 +47,55 @@ func cors(fs http.Handler) http.HandlerFunc {
 
 func handle(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.WriteHeader(200)
 
-	f := getRandFile()
+	if r.RequestURI == "/" {
+		w.WriteHeader(http.StatusAccepted)
+		f, err := getRandFile()
+		if err != nil {
+			panic(err)
+		}
+		w.Write([]byte(f))
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Bad Request"))
+	}
 
-	w.Write([]byte(f))
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	fmt.Println("musicserver")
+
+	f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+
+	go func() {
+		oauth, err := envy.Get("TWITCHAUTH")
+		if err != nil {
+			panic(err)
+		}
+
+		c := twitch.NewClient("shinbot", oauth)
+
+		client = c
+
+		err = c.Connect()
+		if err != nil {
+			panic(err)
+		}
+
+		c.Join("shindakun")
+
+		c.OnUserJoinMessage(func(message twitch.UserJoinMessage) {
+			fmt.Println(message)
+			c.Say("shindakun", "Welcome!")
+		})
+	}()
 
 	fs := http.FileServer(http.Dir("./music"))
 	t := cors(fs)
